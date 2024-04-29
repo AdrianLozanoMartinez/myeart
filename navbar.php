68e2ef955349e961535b8fb471dcd297366ea23f
Adrián Lozano Martínez <lozma.adrian@gmail.com>
2024-04-29 16:00:19 +0200

    Includes hover, image and navbar layout

--- navbar.php
+++ navbar.php
@@ -1,25 +1,25 @@
 <nav class="navbar navbar-expand-lg">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="image/logo.svg"></a>
+  <div class="container-fluid text-center ms-5">
+    <a class="navbar-brand ms-5 mt-3" href="#"><img class="logoNavbar" src="image/logo.svg"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse ms-5" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link text-light" aria-current="page" href="#">OBRAS</a>
+          <a class="nav-link fw-bold text-light" aria-current="page" href="#">OBRAS</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-light" href="#">ARTISTAS</a>
+          <a class="nav-link fw-bold text-light" href="#">ARTISTAS</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-light" aria-current="page" href="#">EVENTOS</a>
+          <a class="nav-link fw-bold text-light" aria-current="page" href="#">EVENTOS</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-light" href="#">MI COMUNIDAD</a>
+          <a class="nav-link fw-bold text-light" href="#">MI COMUNIDAD</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-light" aria-current="page" href="#">COLECTIVOS</a>
+          <a class="nav-link fw-bold text-light" aria-current="page" href="#">COLECTIVOS</a>
         </li>
       </ul>
       <!-- <form class="d-flex" role="search">
