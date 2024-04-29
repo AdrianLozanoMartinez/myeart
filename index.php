eb8b744e0e661452561357a6f733e4bdd902b4b9
Adrián Lozano Martínez <lozma.adrian@gmail.com>
2024-04-29 11:52:55 +0200

    new structure and images

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include 'src/template/navbar.php' ?>
+<?php include 'navbar.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,6 +11,7 @@
     <div class="alert alert-primary" role="alert">
         Myeart - En construcción
     </div>
+    <?php include 'footer.html' ?>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
