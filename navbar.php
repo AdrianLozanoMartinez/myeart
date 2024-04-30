de9121a69822ae01be86c6691ef42ceb87e66ddf
Adrián Lozano Martínez <lozma.adrian@gmail.com>
2024-04-30 11:32:13 +0200

    New page and link with  navbar

--- navbar.php
+++ navbar.php
@@ -1,35 +1,46 @@
-<nav class="navbar navbar-expand-lg">
-  <div class="container-fluid text-center ms-5">
-    <a class="navbar-brand ms-5 mt-3" href="#"><img class="logoNavbar" src="image/logo.svg"></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse ms-5" id="navbarSupportedContent">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid text-center ms-5">
+      <a class="navbar-brand ms-5 mt-3" href="index.php"><img class="logoNavbar" src="image/logo.svg"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse ms-5" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link fw-bold text-light ms-5" aria-current="page" href="constructions.php">OBRAS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-bold text-light ms-3" aria-current="page" href="artists.php">ARTISTAS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-bold text-light ms-3" aria-current="page" href="events.php">EVENTOS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-bold text-light ms-3" aria-current="page" href="myCommunity.php">MY COMUNIDAD</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-bold text-light ms-3" aria-current="page" href="collectives.php">COLECTIVOS</a>
+          </li>
+        </ul>
+      </div>
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link fw-bold text-light ms-5" aria-current="page" href="obras.php">OBRAS</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link fw-bold text-light ms-3" aria-current="page" href="artistas.php">ARTISTAS</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link fw-bold text-light ms-3" aria-current="page" href="eventos.php">EVENTOS</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link fw-bold text-light ms-3" aria-current="page" href="miComunidad.php">MI COMUNIDAD</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link fw-bold text-light ms-3" aria-current="page" href="colectivos.php">COLECTIVOS</a>
-        </li>
-      </ul>
+          <li class="nav-item">
+            <a href="login.php"><button type="button" class="btn text-light loginButton me-1">LOGIN</button></a>
+          </li>
+          <li class="nav-item me-5">
+            <a href="register.php"><button type="button" class="btn text-light loginButton me-5">REGISTRAR</button></a>
+          </li>
+        </ul>
     </div>
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a href="login.php"><button type="button" class="btn text-light loginButton me-1">LOGIN</button></a>
-        </li>
-        <li class="nav-item me-5">
-          <a href="registrar.php"><button type="button" class="btn text-light loginButton me-5">REGISTRAR</button></a>
-        </li>
-      </ul>
-  </div>
-</nav>
+  </nav>
+</body>
+</html>
