5fb4a59c718fbd2a3727cf2e79c0032a7d6d89db
Adrián Lozano Martínez <lozma.adrian@gmail.com>
2024-05-06 13:31:33 +0200

    Complete login design

--- login.php
+++ login.php
@@ -4,16 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="styleLogin.css">
+    <link rel="stylesheet" href="login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
 </head>
 <body>
     <div class="containerLogin">
         <div id="login">
+            <a href="index.php" id="arrowLeft"><span><i class="fa-solid fa-arrow-left"></i></span></a>
             <form  action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
                 <input type="hidden" name="id" value="<?php echo $id; ?>">
-                <span id="arrowLeft"><i class="fa-solid fa-arrow-left"></i></span>
                 <div class="form-group separate">
                     <div class="form-group separate">
                         <span id="emailIcon"><i class='fa-regular fa-envelope fa-lg'></i></span>
@@ -23,9 +23,10 @@
                     </div>
                 </div>
                 <div class="text-center"> 
-                    <button type="submit" class="btn btn-dark buttonLogin">Iniciar sección</button>
+                    <button type="submit" class="btn btn-dark buttonLogin">Iniciar sesión</button>
                 </div>
             </form>
+            <a href="#" id="forget">¿Has olvidado la contraseña?</a>
         </div>
     </div>
     <a href="admin.php">Administrador (Solo para probar, luego se cambia)</a>
